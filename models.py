aef951cfa4cbe29b4b744fceead64f1a02ccb9d8
Cory Trast <cory_trast@hotmail.com>
2020-12-08 15:28:11 -0600

    added required data to model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from peewee import *
+from peewee_validates import ModelValidator
 import datetime
 
 # Connect to a Postgres database.
@@ -6,14 +7,18 @@ DATABASE = PostgresqlDatabase('flask_muse_app', host='localhost', port=5432)
 
 
 class Song(Model):
-    title = CharField()
-    artist = CharField()
-    album = CharField()
+    title = CharField(null=False)
+    artist = CharField(null=False)
+    album = CharField(null=False)
     created_at = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = DATABASE
 
+obj = Song(code=42)
+validator = ModelValidator(obj)
+validator.validate()
+print(validator.errors)
 
 def initialize():
     DATABASE.connect()
